bb90da79dc620499346c556f2efc6b8a1da93095
mahbubk <Mahbub_Khandoker@protonmail.com>
2024-09-16 13:47:23 +0600

    updated form.py

---
+++ url_shortening_app/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.hashers import make_password
+
+from .models import User, ShortenedURL
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Passwords do not match')
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.password = make_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+class ShortenURLForm(forms.ModelForm):
+
+    class Meta:
+        model = ShortenedURL
+        fields = ['original_url', 'custom_short_code', 'expires_at']
+
+    def clean_custom_short_code(self):
+        custom_short_code = self.cleaned_data.get('custom_short_code')
+        if custom_short_code and ShortenedURL.objects.filter(custom_short_code=custom_short_code).exists():
+            raise forms.ValidationError('This short code is already in use')
+        return custom_short_code
