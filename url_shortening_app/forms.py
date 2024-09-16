1def06984009f54711dd20031ceb4495cf2f1545
mahbubk <Mahbub_Khandoker@protonmail.com>
2024-09-16 23:29:40 +0600

    update forms.py

--- url_shortening_app/forms.py
+++ url_shortening_app/forms.py
@@ -10,7 +10,7 @@ class UserRegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email']
 
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
@@ -21,7 +21,8 @@ class UserRegistrationForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.password = make_password(self.cleaned_data['password'])
+        # Set hashed password
+        user.password = make_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
